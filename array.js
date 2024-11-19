97ebbba9e663360f967a20d60ff28a5e79126518
rafiahmedrizvee <rizvirafi7@gmail.com>
2024-11-19 22:30:23 +0600

    basic js array

---
+++ array.js
@@ -0,0 +1,114 @@
+var student = ['Rizvee', 'Rafi', 'Roxas', 'Arafat'];
+//console.log(student.length); //length
+
+var vowel = ['a', 'e', 'i', 'o', 'u']
+//console.log(vowel.length);
+
+var mix = ['Rafi', 26, true, 'Chandpur', 'Student',0182440];
+//console.log(mix.length); length number
+
+const mixArrayIndex = mix.indexOf(255)
+//console.log(mixArrayIndex);  ///index number
+
+var mix = ['Rafi', 26, true, 'Chandpur', 'Student',0182440];
+var findFiveIndex = mix[5];
+//console.log(findFiveIndex); //find index
+
+var findFiveIndex = mix[55];
+//console.log(findFiveIndex);
+
+var mix = ['Rafi', 26, true, 'Chandpur', 'Student',0182440];
+//console.log(mix);
+mix[2] = 'Looking Wow'
+mix.push('Rizvee')
+//console.log(mix); //add new item
+
+var mix = ['Rafi', 26, true, 'Chandpur', 'Student',0182440];
+
+mix.pop()
+mix.pop()
+
+//console.log(mix); //out item 
+
+var mix = ['Rafi', 26, true, 'Chandpur', 'Student',0182440];
+mix.shift();
+//console.log(mix);
+
+
+
+//console.log(10 > 5);
+//console.log(10 < 5);
+//console.log(10 == 5);
+//console.log(10 == 10);
+//console.log(10 != 5);
+//console.log(10 != 10);
+
+
+var myLove = 99;
+var herLove = 100;
+//console.log(myLove > herLove);
+//console.log(myLove == herLove);
+//console.log(myLove < herLove);
+//console.log(myLove != herLove);
+
+// goodStudy && goodPerson
+// hasMoney || handsome || programmer
+
+
+// is food ready == true // khabo
+
+var isFoodReady = true;
+
+if (isFoodReady == true) {
+   //console.log('Ami khabar khabo');
+}
+
+// if statement
+
+var iphone = 160000;
+var myBudget = 90000;
+
+
+if (iphone < myBudget) {
+    console.log('iphone kinbo');
+}
+
+else {
+   // console.log('luck kharaf');
+
+}  ///if /else statement
+
+
+var jobHolder = false;
+var saveMoney = 150000;
+var hasFlat = true;
+
+if (jobHolder == true && saveMoney < 200000){
+   // console.log('biye kora hobe');
+    
+}
+else {
+//    console.log('biye hobe na');
+    
+}
+
+if (jobHolder == true || saveMoney < 200000){
+   // console.log('biye kora hobe');
+     
+ }
+ else {
+  //  console.log('biye hobe na');
+     
+ }
+ 
+
+ if ((jobHolder == true && saveMoney > 200000) || hasFlat == true){
+    console.log('Jao biye kore neo');
+     
+ }
+ else {
+    console.log('biye korar dorkar nai');
+     
+ }
+
+
